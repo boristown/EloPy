b1d3d760fcfc1d0e822dd4205de105c271f90741
AI Era <47687653+boristown@users.noreply.github.com>
2022-12-12 19:54:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,28 @@
 from elopy import *
 
-i = Implementation()
+i = Implementation(base_rating=1000)
 
 i.addPlayer("Hank")
-i.addPlayer("Bill",rating=900)
+i.addPlayer("Boris")
+i.addPlayer("Bill",rating=1200)
 
-print i.getPlayerRating("Hank"), i.getPlayerRating("Bill")
+print(i.getPlayerRating("Hank"), i.getPlayerRating("Boris"), i.getPlayerRating("Bill"))
 
 i.recordMatch("Hank","Bill",winner="Hank")
+i.recordMatch("Hank","Boris",winner="Boris")
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.recordMatch("Hank","Bill",winner="Bill")
+i.recordMatch("Hank","Bill",winner="Bill")
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.recordMatch("Hank","Bill",draw=True)
+i.recordMatch("Hank","Boris",winner="Boris")
 
-print i.getRatingList()
+print(i.getRatingList())
 
 i.removePlayer("Hank")
 
-print i.getRatingList()
+print(i.getRatingList())
